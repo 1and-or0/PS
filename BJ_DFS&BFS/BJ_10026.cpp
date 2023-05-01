1c17291bc7f2770ff5eec0445a52af9a699b7b36
ErrorOrError404 <121109857+ErrorOrError404@users.noreply.github.com>
2023-05-02 01:03:14 +0900

    Update BJ_10026.cpp

--- BJ_DFS&BFS/BJ_10026.cpp
+++ BJ_DFS&BFS/BJ_10026.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 string graph[100];
-char now; // Ž������ ������ R, G, B �� ��� ������ ������
+char now; // 탐색중인 구역이 R, G, B 중 어느 것인지 저장함
 queue<pair<int, int>> q;
 
-//  �Ʒ�, ������, ��, ����
+//  아래, 오른쪽, 위, 왼쪽
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
 
@@ -22,7 +22,7 @@ void bfs()
 
         if (now == 'B')
             graph[x][y] = '.';
-        else // R - G �� ��� K�� �ٲٰ� bfs�� �����ϸ� �������� ���� ������ ����
+        else // R - G 인 경우 K로 바꾸고 bfs를 수행하면 색약인이 보는 구역의 개수
             graph[x][y] = 'K';
 
         for (int i = 0; i < 4; ++i) 
@@ -30,12 +30,12 @@ void bfs()
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            // ������ ��� ���
+            // 범위를 벗어난 경우
             if (nx < 0 || nx >= graph[0].length() || ny < 0 ||
                 ny >= graph[0].length())
                 continue;
 
-            // Ž���ϰ� �ִ� ��尡 ���� ����� ���
+            // 탐색하고 있는 노드가 같은 노드인 경우
             if (now == graph[nx][ny]) 
             {
                 q.push(make_pair(nx, ny));
@@ -62,12 +62,12 @@ void bfs2()
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            // ������ ��� ���
+            // 범위를 벗어난 경우
             if (nx < 0 || nx >= graph[0].length() || ny < 0 ||
                 ny >= graph[0].length())
                 continue;
 
-            // Ž���ϰ� �ִ� ��尡 ���� ����� ���
+            // 탐색하고 있는 노드가 같은 노드인 경우
             if (now == graph[nx][ny])
             {
                 q.push(make_pair(nx, ny));
@@ -84,7 +84,7 @@ int main()
 
     int n;
     cin >> n;
-    // �Է¹ޱ�
+    // 입력받기
     for (int i = 0; i < n; ++i) 
     {
         cin >> graph[i];
@@ -136,3 +136,20 @@ int main()
 
     return 0;
 }
+
+// <낙서장>
+// now = 탐색중인 문자 저장
+// 탐색 시작지점 큐에 넣기 
+// BFS
+// BFS돌면
+// 큐에서 꺼내고 
+// 그 지점 문자 "."로 바꾸고
+// 인접한 곳이 now랑 같은 문자면 탐색 
+// 아니면 탐색X
+// BFS가 끝나면 
+// R구역 개수
+// G구역 개수
+// B구역 개수 중
+// now에 저장된 거에 해당하는 구역의
+// 개수를 하나 올리고 
+// 다음 탐색 시작
