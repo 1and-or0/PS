adedaa5231e15731cbc2b79613b6bbbd19e79553
ErrorOrError404 <pjoon0404@naver.com>
2023-05-06 21:14:50 +0900

    백준

--- BJ_String/BJ_1541try.cpp
+++ BJ_String/BJ_1541try.cpp
@@ -7,12 +7,6 @@ int main()
 {
 	ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	// + 와 - 연산을 기준으로 숫자만을 구분한다
-	// - 연산을 기준으로 왼쪽에 있는 숫자와 오른 쪽에 있는 숫자를 따로 저장한다
-	// 길이가 5면 연산은 최대 2개 수는 최대 3개 길이가 9이면 1 1 1 1 1 6이면 11 1 1
-	// 길이 50 최대로 숫자가 여러번 들어올 경우 25개의 숫자 24개의 연산자
-	// 1+2 - 2+5+2 - 5+2 - 5+2 - 5+6								
-	// 12345 
 	int positive = 0, negative = 0;
 	string str;
 	cin >> str;
@@ -24,48 +18,38 @@ int main()
 		if (str[i] == '+' || str[i] == '-')
 		{
 			if (plusLeft)
-			{
-				addFromString(str.begin()+_index, i - _index, &positive);
-			}
+				addFromString(str.begin() + _index, i - _index, &positive);
 			else
-			{
 				addFromString(str.begin() + _index, i - _index, &negative);
-			}
+
 			_index = i + 1;
 
-			if (str[i] =='-' && plusLeft)
-			{
+			if (str[i] == '-' && plusLeft)
 				plusLeft = false;
-			}
 		}
 		else if (i == str.size() - 1)
 		{
 			if (plusLeft)
-			{
 				addFromString(str.begin() + _index, i - _index + 1, &positive);
-			}
 			else
-			{
 				addFromString(str.begin() + _index, i - _index + 1, &negative);
-			}
 		}
 	}
 
-	cout << positive - negative ;
+	cout << positive - negative;
 	return 0;
 }
 
-void addFromString(string::iterator start, const int len, int * whereAdd)
+void addFromString(string::iterator start, const int len, int* whereAdd)
 {
 	stringstream ss;
 	string num;
 	for (int i = 0; i < len; ++i)
-	{
 		num += start[i];
-	}
+
 	ss << num;
 	int temp = 0;
 	ss >> temp;
-	//cout << temp;
+
 	*whereAdd += temp;
 }
