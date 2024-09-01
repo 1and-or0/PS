ef5db055c9317fa8a5bdf1115fb4ff54afbc3db1
ParkJunWooLT1 <pjoon0404@naver.com>
2024-09-01 21:55:47 +0900

    백준 13460
    BFS PS 재활

---
+++ 백준/백준 3/13460/13460v2.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <queue>
+#include <tuple>
+using namespace std;
+
+struct State {
+    int rx, ry, bx, by, depth;
+};
+
+int N, M;
+char board[10][10];
+bool visited[10][10][10][10];
+int dx[4] = {-1, 1, 0, 0}; // up, down, left, right
+int dy[4] = {0, 0, -1, 1};
+
+bool move(int &x, int &y, int &count, int dx, int dy) 
+{
+    while (board[x + dx][y + dy] != '#' && board[x][y] != 'O') 
+    {
+        x += dx;
+        y += dy;
+        count++;
+        if (board[x][y] == 'O') return true;
+    }
+    return false;
+}
+
+int bfs(int rx, int ry, int bx, int by) 
+{
+    queue<State> q;
+    q.push({rx, ry, bx, by, 0});
+    visited[rx][ry][bx][by] = true;
+
+    while (!q.empty()) 
+    {
+        State cur = q.front();
+        q.pop();
+
+        if (cur.depth >= 10) return -1;
+
+        for (int i = 0; i < 4; ++i) 
+        {
+            int nrx = cur.rx, nry = cur.ry, nbx = cur.bx, nby = cur.by;
+            int rcount = 0, bcount = 0;
+
+            bool red_in_hole = move(nrx, nry, rcount, dx[i], dy[i]);
+            bool blue_in_hole = move(nbx, nby, bcount, dx[i], dy[i]);
+
+            if (blue_in_hole) continue;
+            if (red_in_hole) return cur.depth + 1;
+
+            if (nrx == nbx && nry == nby) 
+            {   
+                if (rcount > bcount) 
+                {
+                    nrx -= dx[i];
+                    nry -= dy[i];
+                } 
+                else 
+                {
+                    nbx -= dx[i];
+                    nby -= dy[i];
+                }
+            }
+
+            if (!visited[nrx][nry][nbx][nby]) 
+            {
+                visited[nrx][nry][nbx][nby] = true;
+                q.push({nrx, nry, nbx, nby, cur.depth + 1});
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main() 
+{
+    cin >> N >> M;
+    int rx, ry, bx, by;
+
+    for (int i = 0; i < N; ++i) 
+    {
+        for (int j = 0; j < M; ++j) 
+        {
+            cin >> board[i][j];
+            if (board[i][j] == 'R')
+            {
+                rx = i;
+                ry = j;
+            } 
+            else if (board[i][j] == 'B') 
+            {
+                bx = i;
+                by = j;
+            }
+        }
+    }
+
+    cout << bfs(rx, ry, bx, by) << endl;
+    return 0;
+}
