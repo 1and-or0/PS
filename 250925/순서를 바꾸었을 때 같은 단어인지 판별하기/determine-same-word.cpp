bf4c4eff0fdb76c89a77eb06d4bf064df2479c06
박준우 <121109857+1and-or0@users.noreply.github.com>
2025-09-25 00:52:16 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 13ms, Memory: 1MB, Status: Passed - Codetree

--- 250925/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
+++ 250925/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -7,29 +7,58 @@ using namespace std;
 string word1;
 string word2;
 
+int iAlphabetOrNum(char c)
+{
+    int result = 0;
+    if (c >= '0' && c <= '9')
+    {
+        result =  (int ) c - '0';
+    }
+    else if (c >= 'A' && c <= 'Z')
+    {
+        result = (int) (c - 'A') + 10;
+    }
+    else
+    {
+        result = (int)(c - 'a') + 10 + 26;
+    }
+
+    return result;
+}
+
 int main()
 {
     cin >> word1;
     cin >> word2;
 
     // Please write your code here.
-    sort(word1.begin(), word1.end());
-    sort(word2.begin(), word2.end());
-
+    // 0~9 + A~Z + a~z 각 개수 저장
+    const int size = 10 + (int)('Z'-'A') + 1 + (int)('z' - 'a') + 1;
+    
+    int cnt1[size] = { 0 };
+    int cnt2[size] = { 0 };
     bool isNo = false;
-
-    for (int i = 0; i < min(word1.length(), word2.length()); i++)
-    {
-        if (word1[i] != word2[i])
-        {
-            isNo = true;
-            break;
-        }
-    }
-
+    int n = max(word1.length(), word2.length());
     if (word1.length() != word2.length())
     {
         isNo = true;
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cnt1[iAlphabetOrNum(word1[i])] += 1;
+            cnt2[iAlphabetOrNum(word2[i])] += 1;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            if (cnt1[i] != cnt2[i])
+            {
+                isNo = true;
+                break;
+            }
+        }
     }
 
     if (isNo)
