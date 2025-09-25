e85be58a16788d5c5405ba78a7f0f13476b970dd
박준우 <121109857+1and-or0@users.noreply.github.com>
2025-09-26 02:02:25 +0900

    Title: 최대로 겹치는 구간, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250926/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 250926/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -21,8 +21,8 @@ int main()
     int n;
     cin >> n;
 
-    const int MAX_X = 999999; // 좌표 범위 (6자리 정수 가정)
-    vector<int> diff(MAX_X + 2, 0);
+    const int MAX_X = 100; // 좌표 범위 (6자리 정수 가정)
+    vector<int> diff(2*MAX_X + 2, 0);
 
     // 입력 구간 반영
     for (int i = 0; i < n; i++)
@@ -30,8 +30,8 @@ int main()
         int x1, x2;
         cin >> x1 >> x2;
 
-        diff[x1] += 1;   // 구간 시작점에서 +1
-        diff[x2] -= 1;   // 구간 끝 다음 좌표에서 -1
+        diff[x1 + 100] += 1;   // 구간 시작점에서 +1
+        diff[x2 + 100] -= 1;   // 구간 끝 다음 좌표에서 -1
     }
 
     int curr = 0;
@@ -41,19 +41,19 @@ int main()
     bool in_max_interval = false;
 
     // 좌표 순회 (Counting Sort처럼 좌표 범위 전체를 순차 확인)
-    for (int x = 0; x <= MAX_X; x++)
+    for (int x = 0; x < MAX_X* 2 + 1; x++)
     {
         curr += diff[x];
 
         if (curr > max_cnt)
         {
             max_cnt = curr;
-            best_start = x;
+            best_start = x - 100;
             in_max_interval = true;
         }
         else if (in_max_interval && curr < max_cnt)
         {
-            best_end = x;
+            best_end = x - 100;
             in_max_interval = false;
         }
     }
